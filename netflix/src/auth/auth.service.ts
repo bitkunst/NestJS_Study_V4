69aa4e9c1a022c2125f75dcea066df7d74d28d59
bitkunst <bitkunst23@gmail.com>
2025-01-29 21:35:27 +0900

    feat: passport local strategy

--- netflix/src/auth/auth.service.ts
+++ netflix/src/auth/auth.service.ts
@@ -18,12 +18,7 @@ export class AuthService {
     async login(rawToken: string) {
         // raw token -> "Basic $token"
         const { email, password } = this.parseBasicToken(rawToken);
-
-        const user = await this.userRepository.findOne({ where: { email } });
-        if (!user) throw new BadRequestException('잘못된 로그인 정보입니다!');
-
-        const passOk = await bcrypt.compare(password, user.password);
-        if (!passOk) throw new BadRequestException('잘못된 로그인 정보입니다!');
+        const user = await this.authenticate(email, password);
 
         const refreshTokenSecret = this.configService.get<string>('REFRESH_TOKEN_SECRET');
         const accessTokenSecret = this.configService.get(<string>'ACCESS_TOKEN_SECRET');
@@ -63,6 +58,16 @@ export class AuthService {
         await this.userRepository.save({ email, password: hash });
 
         return this.userRepository.findOne({ where: { email } });
+    }
+
+    async authenticate(email: string, password: string) {
+        const user = await this.userRepository.findOne({ where: { email } });
+        if (!user) throw new BadRequestException('잘못된 로그인 정보입니다!');
+
+        const passOk = await bcrypt.compare(password, user.password);
+        if (!passOk) throw new BadRequestException('잘못된 로그인 정보입니다!');
+
+        return user;
     }
 
     parseBasicToken(rawToken: string) {
