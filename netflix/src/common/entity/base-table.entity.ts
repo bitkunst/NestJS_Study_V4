8342a28fc83ffd1cdf5e6aa247369b4a269ed234
bitkunst <bitkunst23@gmail.com>
2025-01-29 17:18:57 +0900

    refactor: apply @Exclude class-transformer

--- netflix/src/common/entity/base-table.entity.ts
+++ netflix/src/common/entity/base-table.entity.ts
@@ -1,12 +1,16 @@
+import { Exclude } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 export class BaseTable {
+    @Exclude()
     @CreateDateColumn()
     createdAt: Date;
 
+    @Exclude()
     @UpdateDateColumn()
     updatedAt: Date;
 
+    @Exclude()
     @VersionColumn()
     version: number;
 }
