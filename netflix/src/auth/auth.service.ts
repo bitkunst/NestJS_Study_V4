085b36073f3a56f0865c43d4a2ad1c65ecd0baa0
bitkunst <bitkunst23@gmail.com>
2025-01-29 23:12:10 +0900

    feat: rotate access token route

--- netflix/src/auth/auth.service.ts
+++ netflix/src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/user/entity/user.entity';
+import { Role, User } from 'src/user/entity/user.entity';
 import { Repository } from 'typeorm';
 import bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
@@ -43,7 +43,7 @@ export class AuthService {
         return this.userRepository.findOne({ where: { email } });
     }
 
-    async issueToken(user: User, isRefreshToken: boolean) {
+    async issueToken(user: { id: number; role: Role }, isRefreshToken: boolean) {
         const refreshTokenSecret = this.configService.get<string>('REFRESH_TOKEN_SECRET');
         const accessTokenSecret = this.configService.get(<string>'ACCESS_TOKEN_SECRET');
 
@@ -76,7 +76,8 @@ export class AuthService {
         const basicSplit = rawToken.split(' ');
         if (basicSplit.length !== 2) throw new BadRequestException('토큰 포맷이 잘못됐습니다!');
 
-        const [, token] = basicSplit;
+        const [basic, token] = basicSplit;
+        if (basic.toLowerCase() !== 'basic') throw new BadRequestException('토큰 포맷이 잘못됐습니다!');
 
         // 2) 추출한 토큰을 base64 디코딩해서 이메일과 비밀번호로 나눈다
         const decoded = Buffer.from(token, 'base64').toString('utf-8');
@@ -89,5 +90,28 @@ export class AuthService {
         const [email, password] = tokenSplit;
 
         return { email, password };
+    }
+
+    async parseBearerToken(rawToken: string, isRefreshToken: boolean) {
+        const bearerSplit = rawToken.split(' ');
+        if (bearerSplit.length !== 2) throw new BadRequestException('토큰 포맷이 잘못됐습니다!');
+
+        const [bearer, token] = bearerSplit;
+        if (bearer.toLowerCase() !== 'bearer') throw new BadRequestException('토큰 포맷이 잘못됐습니다!');
+
+        const refreshTokenSecret = this.configService.get<string>('REFRESH_TOKEN_SECRET');
+        const accessTokenSecret = this.configService.get(<string>'ACCESS_TOKEN_SECRET');
+
+        const payload = await this.jwtService.verifyAsync(token, {
+            secret: isRefreshToken ? refreshTokenSecret : accessTokenSecret,
+        });
+
+        if (isRefreshToken) {
+            if (payload.type !== 'refresh') throw new BadRequestException('Refresh 토큰을 입력해주세요!');
+        } else {
+            if (payload.type !== 'access') throw new BadRequestException('Access 토큰을 입력해주세요!');
+        }
+
+        return payload;
     }
 }
