e4f58479556cbab298a51375b641270c13b118db
bitkunst <bitkunst23@gmail.com>
2025-06-08 02:54:59 +0900

    feat: query-failed exception filter

--- netflix/src/app.module.ts
+++ netflix/src/app.module.ts
@@ -15,6 +15,7 @@ import { AuthGuard } from './auth/guard/auth.guard';
 import { RBACGuard } from './auth/guard/rbac.guard';
 import { ResponseTimeInterceptor } from './common/interceptor/response-time.interceptor';
 import { ForbiddenExceptionFilter } from './common/filter/forbidden.filter';
+import { QueryFailedExceptionFilter } from './common/filter/query-failed.filter';
 
 /**
  * @dev
@@ -77,6 +78,10 @@ import { ForbiddenExceptionFilter } from './common/filter/forbidden.filter';
         {
             provide: APP_FILTER,
             useClass: ForbiddenExceptionFilter,
+        },
+        {
+            provide: APP_FILTER,
+            useClass: QueryFailedExceptionFilter,
         },
     ],
 })
