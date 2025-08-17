a27fd9e4737cab5ebe0f4984de02692458d06179
bitkunst <bitkunst23@gmail.com>
2025-08-18 00:05:12 +0900

    feat: file-uploads with multer (diskStorage)

--- netflix/src/movie/movie.controller.ts
+++ netflix/src/movie/movie.controller.ts
@@ -11,6 +11,9 @@ import {
     ClassSerializerInterceptor,
     ParseIntPipe,
     Request,
+    UploadedFile,
+    UploadedFiles,
+    BadRequestException,
 } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
@@ -20,6 +23,7 @@ import { RBAC } from 'src/auth/decorator/rbac.decorator';
 import { Role } from 'src/user/entity/user.entity';
 import { GetMoviesDto } from './dto/get-movies.dto';
 import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
+import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('movie')
 @UseInterceptors(ClassSerializerInterceptor) // class-transformer 적용
@@ -40,8 +44,38 @@ export class MovieController {
 
     @RBAC(Role.ADMIN)
     @UseInterceptors(TransactionInterceptor)
+    @UseInterceptors(
+        FileFieldsInterceptor(
+            [
+                // name: {필드명}, maxCount: {최대파일개수}
+                { name: 'movie', maxCount: 1 },
+                { name: 'poster', maxCount: 2 },
+            ],
+            {
+                // Multer Options
+                limits: {
+                    fileSize: 20000000, // 20MB
+                },
+                fileFilter(req, file, callback) {
+                    if (file.mimetype !== 'video/mp4') {
+                        return callback(new BadRequestException('MP4 타입만 업로드 가능합니다!'), false);
+                    }
+                    callback(null, true); // callback() 함수의 파라미터로 "에러", "파일 수신 여부" 전달
+                },
+            },
+        ),
+    )
     @Post()
-    postMovie(@Body() body: CreateMovieDto, @Request() req) {
+    postMovie(
+        @Body() body: CreateMovieDto,
+        @Request() req,
+        @UploadedFiles()
+        files: {
+            movie?: Express.Multer.File[];
+            poster?: Express.Multer.File[];
+        },
+    ) {
+        console.log('files', files);
         return this.movieService.create(body, req.queryRunner);
     }
 
