266cbf47d1f76d794fe2e7bf7f6a13e4721b142d
bitkunst <bitkunst23@gmail.com>
2025-10-08 03:40:10 +0900

    feat: NestJS default logger

--- netflix/src/common/tasks.service.ts
+++ netflix/src/common/tasks.service.ts
@@ -5,18 +5,27 @@ import { readdir, unlink } from 'fs/promises';
 import path from 'path';
 import { Movie } from 'src/movie/entity/movie.entity';
 import { Repository } from 'typeorm';
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class TasksService {
+    private readonly logger = new Logger(TasksService.name);
+
     constructor(
         @InjectRepository(Movie)
         private readonly movieRepository: Repository<Movie>,
         private readonly schedulerRegistry: SchedulerRegistry,
     ) {}
 
-    // @Cron('* * * * * *')
+    // @Cron('*/5 * * * * *')
     logEverySecond() {
-        console.log('1초마다 실행!');
+        // NestJS에서 제공해주는 로그 레벨 순서
+        this.logger.fatal('FATAL 레벨 로그'); // 지금 당장 해결해야 하는 문제 관련 로그
+        this.logger.error('ERROR 레벨 로그'); // 에러 로그
+        this.logger.warn('WARN 레벨 로그'); // 경고성 로그 (프로그램 실행에는 문제가 되지 않지만, 일어나면 안 좋은 상황)
+        this.logger.log('LOG 레벨 로그'); // 정보성 로그 (INFO 레벨)
+        this.logger.debug('DEBUG 레벨 로그'); // 개발환경에서 중요한 로그
+        this.logger.verbose('VERBOSE 레벨 로그'); // 세밀한 추적/진단 로그
     }
 
     // @Cron('*/5 * * * * *')
