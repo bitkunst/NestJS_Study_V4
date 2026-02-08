3949bb91642b95fa081092deff4ae6315b56487c
bitkunst <bitkunst23@gmail.com>
2026-02-08 18:41:17 +0900

    feat: configure BullMQ options

--- netflix/src/common/common.controller.ts
+++ netflix/src/common/common.controller.ts
@@ -32,10 +32,21 @@ export class CommonController {
     )
     async createVideo(@UploadedFile() video: Express.Multer.File) {
         // Producer가 큐에 넣은 파라미터를 가지고 Consumer가 작업을 할 수 있는 정도의 정보를 넣어줘야 된다
-        await this.thumbnailQueue.add('thumbnail', {
-            videoId: video.filename,
-            videoPath: video.path,
-        });
+        await this.thumbnailQueue.add(
+            'thumbnail',
+            {
+                videoId: video.filename,
+                videoPath: video.path,
+            },
+            {
+                priority: 1, // 숫자가 낮을수록 우선순위 높음
+                delay: 100, // 100ms 뒤에 작업 실행
+                attempts: 3, // 작업 실패 시 최대 3번 재시도
+                lifo: false, // 작업 실행 순서 지정 (true: 최근 작업 우선, false: 오래된 작업 우선) -> true일 경우 Stack 구조로 작업 실행
+                removeOnComplete: true, // 작업 완료 시 작업 정보 삭제
+                removeOnFail: true, // 작업 실패 시 작업 정보 삭제
+            },
+        );
 
         return {
             fileName: video.filename,
